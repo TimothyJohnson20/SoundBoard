bea7c8614e5a36ceab4161822085d07106999997
Timothy Johnson <timothyjohnsontimothy@gmail.com>
2019-09-26 13:56:04 -0700

    Finished 2 songs

--- app/src/main/java/com/example/soundboard/SoundBoardActivity.java
+++ app/src/main/java/com/example/soundboard/SoundBoardActivity.java
@@ -5,14 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextClock;
 import android.widget.TextView;
-
-import com.example.soundboard.R;
-
-import java.lang.reflect.Array;
 
 public class SoundBoardActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,12 +26,16 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
     public Button buttonGSharp;
     private Button buttonNext;
     private Button buttonBack;
-    private Button song1;
+    private Button scale;
     private TextView pitch;
     private SoundPool soundPool;
     private double rateChange = 1;
     private int octave;
     private int[] sounds;
+    private int[] soundsscale;
+    private Button furElise;
+    private Button megalovania;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +46,7 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
         soundArray();
         pitch.setText("1");
         octave = 1;
+        scaleArray();
     }
     public void setListeners(){
         buttonA.setOnClickListener(this);
@@ -76,7 +77,42 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
                 pitch.setText(octave + "");
             }
         });
+        scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playScale();
+            }
+        });
+        furElise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playFurElise();
+            }
+        });
+        megalovania.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playMegalovania();
+            }
+        });
     }
+
+    private void playScale(){
+        for(int i = 0; i < 12; i++){
+            soundPool.play(soundsscale[i], 1, 1, 1, 0, (float) rateChange);
+            delay(300);
+                }
+
+        }
+    private void delay(int millis){
+        try{
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e1){
+            e1.printStackTrace();
+        }
+    }
+
     private void soundArray(){
         sounds = new int[12];
         sounds[0] = soundPool.load(getApplicationContext(), R.raw.scalea, 1);
@@ -92,6 +128,44 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
         sounds[10] = soundPool.load(getApplicationContext(), R.raw.scalefs, 1);
         sounds[11] = soundPool.load(getApplicationContext(), R.raw.scalegs, 1);
     }
+    private void scaleArray(){
+        soundsscale = new int[12];
+        soundsscale[0] = soundPool.load(getApplicationContext(), R.raw.scalea, 1);          //  A       0
+        soundsscale[1] = soundPool.load(getApplicationContext(), R.raw.scalebb, 1);         //  A#      1
+        soundsscale[2] = soundPool.load(getApplicationContext(), R.raw.scaleb, 1);          //  B       2
+        soundsscale[3] = soundPool.load(getApplicationContext(), R.raw.scalec, 1);          //  C       3
+        soundsscale[4] = soundPool.load(getApplicationContext(), R.raw.scalecs, 1);         //  C#      4
+        soundsscale[5] = soundPool.load(getApplicationContext(), R.raw.scaled, 1);          //  D       5
+        soundsscale[6] = soundPool.load(getApplicationContext(), R.raw.scaleds, 1);         //  D#      6
+        soundsscale[7] = soundPool.load(getApplicationContext(), R.raw.scalee, 1);          //  E       7
+        soundsscale[8] = soundPool.load(getApplicationContext(), R.raw.scalef, 1);          //  F       8
+        soundsscale[9] = soundPool.load(getApplicationContext(), R.raw.scalefs, 1);         //  F#      9
+        soundsscale[10] = soundPool.load(getApplicationContext(), R.raw.scaleg, 1);         //  G       10
+        soundsscale[11] = soundPool.load(getApplicationContext(), R.raw.scalegs, 1);        //  G#      11
+    }
+
+    private void playFurElise(){
+        int[] furElise = {7, 6, 7, 6, 7, 2, 5, 3, 0, 3, 7, 0, 3, 7, 0, 2, 3, 7, 6, 7, 6, 7, 2, 5, 3, 0, 3, 7, 0, 3, 7, 3, 2, 0, 2, 3, 5, 7, 10, 8, 7, 5, 7
+        , 7, 5, 3, 7, 5, 3, 7, 7, 6, 7, 6, 7, 7, 5, 3, 0, 3, 7, 0, 2, 7, 0, 2, 3, 7, 6, 7, 6, 7, 2, 5, 3, 0, 3, 7, 0, 2, 7, 3, 2, 0};
+        int length = furElise.length;
+        for(int i = 0; i < length; i++){
+            soundPool.play(soundsscale[furElise[i]], 1, 1, 1, 0, (float)rateChange);
+            delay(200);
+        }
+    }
+    private void playMegalovania(){
+        int[] megalovania = {5, 0, 11, 10, 8, 5, 8, 10, 3, 3};
+        int length = megalovania.length;
+        for(int c = 0; c < 5; c++){
+            for(int i = 0; i < length; i ++){
+                soundPool.play(soundsscale[megalovania[i]], 1, 1, 1, 0, (float)rateChange);
+                delay(200);
+            }
+            rateChange = rateChange + .8;
+        }
+
+    }
+
     public void wireWidgets(){
         buttonA = findViewById(R.id.button_soundboard_a);
         buttonB = findViewById(R.id.button_soundboard_b);
@@ -108,7 +182,9 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
         buttonNext = findViewById(R.id.button_soundboard_next);
         buttonBack = findViewById(R.id.button_soundboard_back);
         pitch = findViewById(R.id.textView_main_pitch);
-        song1 = findViewById(R.id.button_soundboard_song1)
+        scale = findViewById(R.id.button_soundboard_scale);
+        furElise = findViewById(R.id.button_soundboard_furElise);
+        megalovania = findViewById(R.id.button_soundboard_megalovania);
     }
 
     @Override
